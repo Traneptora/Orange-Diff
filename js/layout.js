eef80697249081455982302c668f2b39ec8bb11e
Leo Izen <leo.izen@gmail.com>
2017-11-20 14:03:31 -0500

    inital code commit

---
+++ js/layout.js
@@ -0,0 +1,85 @@
+function parseURLParams(url) {
+	var queryStart = url.indexOf("?") + 1,
+		queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+		query = url.slice(queryStart, queryEnd - 1),
+		pairs = query.split("&"),
+		parms = {}, i, n, v, nv;
+
+	if (query === url || query === "") {
+		return [];
+	}
+
+	for (i = 0; i < pairs.length; i++) {
+		nv = pairs[i].split("=");
+		if (nv.length !== 2){
+			continue;
+		}
+		n = decodeURIComponent(nv[0]);
+		v = decodeURIComponent(nv[1]);
+		parms[n] = v;
+	}
+	return parms;
+}
+
+function setupDiffLayout(imageAURL, imageBURL){
+	$wrapperdiv = $("<div></div>");
+	$wrapperdiv.css({
+		"margin-left": "25px",
+		"margin-right": "25px",
+		"margin-top": "25px",
+		"margin-bottom": "25px",
+		"border": "0px",
+		"padding": "0px"
+	});
+	$htag = $("<h4>Image A</h4>");
+	$imgtag = $("<img>");
+	$imgtag.css({
+		"border": "0px",
+		"padding": "0px",
+		"margin": "0px"
+	});
+	$imgtag.prop("src", imageAURL);
+	$imgtag.on("mouseover", function(){
+		$(this).prop("src", imageBURL);
+		$htag.prop("innerHTML", "Image B");
+	});
+	$imgtag.on("mouseout", function(){
+		$(this).prop("src", imageAURL);
+		$htag.prop("innerHTML", "Image A");
+	});
+	$wrapperdiv.append($htag);
+	$wrapperdiv.append($imgtag);
+    $
+	$(document.body).css({
+		"padding": "0px",
+		"margin": "0px",
+		"border": "0px",
+        "text-align": "left"
+	});
+	document.body.innerHTML = "";
+
+	$(document.body).append($wrapperdiv);
+}
+
+function submitDiff(){
+	var imageAURL = $("#imageatextfield").prop("value");
+	var imageBURL = $("#imagebtextfield").prop("value");
+	if (imageAURL && imageBURL && imageAURL !== "" && imageBURL !== ""){
+		var imageAURLEncoded = encodeURIComponent(imageAURL);
+		var imageBURLEncoded = encodeURIComponent(imageBURL);
+		window.location.search = "?imagea=" + imageAURLEncoded + "&imageb=" + imageBURLEncoded;
+		setupDiffLayout(imageAURL, imageBURL);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+	var params = parseURLParams(window.location.search);
+	if (params.imagea && params.imageb){
+		if (params.nogen && params.nogen === "true"){
+			$("#imageatextfield").prop("value", params.imagea);
+			$("#imagebtextfield").prop("value", params.imageb);
+		} else {
+			setupDiffLayout(params.imagea, params.imageb);
+		}
+	}
+});
